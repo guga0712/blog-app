cd395a6814bdcdcd9ad8fc3c41f4f2aad211748e
guga0712 <martins0712@hotmail.com>
2022-06-25 00:04:41 -0300

    changed social buttons style

--- src/components/AboutUs/Socials.tsx
+++ src/components/AboutUs/Socials.tsx
@@ -11,9 +11,12 @@ export default function Socials({ github, linkedin, facebook }: SocialsProps) {
     return (
         <>
             <Flex width="80%" justifyContent="center" gap="20px">
-                <Link href={github} target="_blank" color="cyan.500"><FaGithub size="30px" /></Link>
-                <Link href={linkedin} target="_blank" color="cyan.500"><FaLinkedin size="30px" /></Link>
-                <Link href={facebook} target="_blank" color="cyan.500"><FaFacebook size="30px" /></Link>
+                {/* <Link href={github} target="_blank" color="cyan.500" _hover={{ color: "cyan.600" }}><FaGithub size="30px" /></Link>
+                <Link href={linkedin} target="_blank" color="cyan.500" _hover={{ color: "cyan.600" }}><FaLinkedin size="30px" /></Link>
+                <Link href={facebook} target="_blank" color="cyan.500" _hover={{ color: "cyan.600" }}><FaFacebook size="30px" /></Link> */}
+                <Link href={github} target="_blank" bg="cyan.500" p="5px" borderRadius="5px" color="white" _hover={{ bg: "cyan.600" }}><FaGithub size="30px" /></Link>
+                <Link href={linkedin} target="_blank" bg="cyan.500" p="5px" borderRadius="5px" color="white" _hover={{ bg: "cyan.600" }}><FaLinkedin size="30px" /></Link>
+                <Link href={facebook} target="_blank" bg="cyan.500" p="5px" borderRadius="5px" color="white" _hover={{ bg: "cyan.600" }}><FaFacebook size="30px" /></Link>
             </Flex>
         </>
 
